8936697cd74f381954cf81c3473356f67859bdaf
Elaine Jackson-Pimentel <me@elaine.is>
2025-01-09 17:04:24 -0500

    Update account implemented:

--- src/index.ts
+++ src/index.ts
@@ -159,8 +159,61 @@ async function verifyCredentials(request: Request, params: Record<string, string
 	}, { status: 200 });
 }
 
-function updateAccount(request: Request, params: Record<string, string>, env: Env) {
-	return new Response("Not implemented!", { status: 501 });
+async function updateAccount(request: Request, params: Record<string, string>, env: Env) {
+	if (!await isUserLoggedIn(request)) {
+		return new Response("Unauthorized", { status: 401 });
+	}
+
+	const userId = await getUserIdFromJwt(request);
+
+	const user = await env.DB.prepare(
+		`
+		SELECT * FROM Users
+		WHERE Id = ?
+		`
+	).bind(userId).first();
+
+	if (!user) {
+		return new Response("User not found", { status: 404 });
+	}
+
+	const inputSchema = z.object({
+		displayName: z.string(),
+		email: z.string().email(),
+		password: z.string().min(16),
+	});
+
+	const contentType = request.headers.get("content-type");
+
+	if (contentType && contentType.includes("application/json")) {
+		try {
+			const json = await request.json();
+			const parsedInput = inputSchema.parse(json);
+			const { displayName, email, password } = parsedInput;
+			const passwordHash = hashSync(password, 8);
+
+			const updateResult = await env.DB.prepare(
+				`
+				UPDATE Users
+				SET Username = ?,
+				EmailAddress = ?,
+				PasswordHash = ?
+				WHERE Id = ?
+				`
+			)
+			.bind(displayName, email, passwordHash, userId)
+			.run();
+
+			return new Response("Account updated", { status: 200 });
+		} catch (error) {
+			if (error instanceof z.ZodError) {
+				return new Response(JSON.stringify(error.errors), { status: 400 });
+			}
+			return new Response("Invalid JSON payload!", { status: 400 });
+		}
+	} else {
+		return new Response("Invalid content-type!");
+	}
 }
 
 async function getCategories(request: Request, params: Record<string, string>, env: Env) {
