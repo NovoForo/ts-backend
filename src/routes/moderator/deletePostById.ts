ab734a217a0fdbb1d7a98bfd6a9067f67de80aa0
Elaine Jackson-Pimentel <me@elaine.is>
2025-01-13 21:38:09 -0500

    Improve delete post handling

--- src/routes/moderator/deletePostById.ts
+++ src/routes/moderator/deletePostById.ts
@@ -12,15 +12,63 @@ async function deletePostById(request: Request, params: Record<string, string>, 
 
     const postId = params.postId;
 
-    await env.DB.prepare(
-        `
-        DELETE FROM Posts
-        WHERE Id = ?
-        `
-    )
-    .bind(postId)
-    .run();
+    // Get Post
+    const post = await env.DB.prepare(`SELECT * FROM Posts WHERE Id = ?`)
+        .bind(postId)
+        .first();
 
+    // Get Topic
+    const topic = await env.DB.prepare(`SELECT * FROM Topics WHERE Id = ?`)
+        .bind(post.TopicId)
+        .first()
+
+    
+    // Count Posts in Topic
+    const count = await env.DB.prepare(`
+        SELECT COUNT(*) AS postCount
+        FROM Posts
+        WHERE TopicId = ?
+        `).bind(post.TopicId).first();
+
+    if (count && count.postCount && count.postCount as number > 0) {
+        // There are multiple posts (only delete this post)
+            await env.DB.prepare(
+                `
+                DELETE FROM Posts
+                WHERE Id = ?
+                `
+            )
+            .bind(postId)
+            .run();
+    } else {
+        // There is only one post (also delete the topic)
+        await env.DB.prepare(
+            `
+            DELETE FROM Posts
+            WHERE Id = ?
+            `
+        )
+        .bind(postId)
+        .run();
+        }.then(() => {
+            await env.DB.prepare(
+            `
+            DELETE FROM Topics
+            WHERE Id = ?
+            `
+        )
+        .bind(post.TopicId)
+        .run().then(() => {
+                await env.DB.prepare(
+                    `
+                    DELETE FROM Topics
+                    WHERE Id = ?
+                    `
+                )
+                .bind(post.TopicId)
+                .run();
+            })
+        })
     return new Response("Post deleted", { status: 200 });
 }
 
